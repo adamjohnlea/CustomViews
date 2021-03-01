6046929abb60bc04babc2ed736d1041907936b1c
adamjohnlea <imnogeekdptcom@me.com>
2021-02-28 18:58:17 -0700

    Initial Commit

---
+++ CustomViews/CustomViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewsApp.swift
+//  CustomViews
+//
+//  Created by Adam Lea on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
